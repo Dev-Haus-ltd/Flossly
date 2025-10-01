1095e37885cd80e93b2d2a93bd3353a04dc58e35
Umair Masood <uwinning50@gmail.com>
2025-10-01 16:11:26 +0500

    left bar bg. selected item bg color, border color and appbar icon with title updated

--- components/appBar/appBar.vue
+++ components/appBar/appBar.vue
@@ -11,14 +11,28 @@
             : 'logo-parent-sm pl-3 bg-secondary'
         "
       >
-        <img
-          :src="!rail ? headerLogo : logoIcon"
-          alt="My Logo"
-          max-height="40"
-          :width="!rail ? '107px' : '30px'"
-          height="100%"
-          contain
-        />
+        <div class="logo-wrapper">
+          <!-- Expanded state: logo + title -->
+          <template v-if="!rail">
+            <img
+              :src="headerLogo"
+              alt="My Logo"
+              width="35"
+              height="100%"
+            />
+            <span class="dashboard-title">Flossly Dashboard</span>
+          </template>
+
+          <!-- Collapsed state: only icon -->
+          <template v-else>
+            <img
+              :src="logoIcon"
+              alt="My Logo"
+              width="30"
+              height="100%"
+            />
+          </template>
+        </div>
       </div>
     </template>
     <!-- Middle: Spacer to push icons right -->
@@ -54,7 +68,7 @@
 
 <script setup>
 const { user } = useUser();
-import headerLogo from "@/assets/logos/headerLogo.svg";
+import headerLogo from "@/assets/logos/logoIcon2.svg";
 import logoIcon from "@/assets/logos/logoIcon2.svg";
 const emit = defineEmits(["small-screen-drawer"]);
 
@@ -80,16 +94,34 @@ watch(
   width: 255px;
   /* background-color: black; */
   height: 100%;
+  margin-left: 20px; /* spacing from logo */
 }
 .logo-parent-sm {
   width: 56px;
   /* background-color: black; */
   height: 100%;
+  margin-left: 20px; /* spacing from logo */
 }
 .cust-border {
   border-bottom: 1px solid #dbdbdb;
 }
 ::v-deep(.v-toolbar__prepend) {
   margin-inline: 0 !important;
+}
+.logo-wrapper {
+  display: flex;
+  align-items: center; /* align title vertically with logo */
+  height: 100%;
+}
+
+/* Title typography */
+.dashboard-title {
+  height: 26px;
+  font-family: "Inter", sans-serif;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 130%; /* ~26px */
+  color: #ffffff;
+  margin-left: 20px; /* spacing from logo */
 }
 </style>
