f3e55032654b4bd6e07d5dd482574c5fd3578abe
Usama Naeem <usama.naeem@31g.co.uk>
2025-10-06 13:31:10 +0500

    terms and conditions updated

--- components/privacyPolicy/index.vue
+++ components/privacyPolicy/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <h1 class="text-center" style="font-weight: 600; font-size: 46px">
-      Privacy & policy
+      FlosslyOS AI Powered Task Manager & CRM Software Terms of Use
     </h1>
     <v-card-text
       style="
@@ -13,117 +13,262 @@
       "
     >
       <p>
-        This Transfer Agreement ("Agreement") is entered into between Smart
-        Compliance Training Limited (“Smart Dental”), Compliance Health Limited
-        (“Dental Compliance Made Easier”, or “DCME”) and the undersigned client
-        ("Client") as of the date signed below.
+        These Flossly Terms of Use (“Terms”) apply to your use of the Flossly AI
+        Powered Task Manager & CRM software (“Platform”) and constitute an
+        agreement between FlosslyOS Ltd. (“Flossly,” “we,” “us”) and you, either
+        individually or on behalf of your employer or another entity you
+        represent, and govern your use of the Platform and all information on or
+        submitted through it.
       </p>
       <p>
-        Smart Dental has entered into an arrangement with DCME and is
-        transferring all subscriptions and customer data to DCME as part of a
-        service migration. THEREFORE, in consideration of the mutual promises
-        and covenants set forth herein, the parties agree as follows:
+        Your use of the Platform constitutes your agreement to these Terms,
+        including but not limited to our Privacy Policy, as amended from time to
+        time. Do not use the Platform if you do not agree to these Terms or if
+        your jurisdiction will not honor them.
+      </p>
+      <p>
+        References to “you” mean the individual user unless otherwise stated.
+        You represent that after reading and understanding these Terms, you
+        agree to be bound by them. Persons under 18 years of age are not
+        authorized to use the Platform.
       </p>
 
       <!-- Section 1 -->
-      <h3 class="terms-heading mt-6">1. Transfer of Subscription & Services</h3>
+      <h3 class="terms-heading mt-6">1. Platform Registration</h3>
       <p>
-        Effective immediately upon signing this Agreement and a DCME agreement
-        (provided separately to this document), Client's subscription to the
-        services provided by Smart Dental will be transferred to DCME. However,
-        Smart Dental will continue to provide services on a business-as-usual
-        basis until the Client is fully onboarded to DCME systems and processes.
+        You may need to complete a registration or create a profile to access
+        and use the Platform. If registration requires username, password, or
+        other identifier, you agree to keep these confidential. You must provide
+        accurate, current, and complete information and keep this updated
+        accordingly. Inaccurate or incomplete information may lead to suspension
+        or termination of access, and we reserve all rights and remedies as
+        permitted by law.
       </p>
       <p>
-        The Client acknowledges and agrees that DCME will handle all future
-        subscription payments, billing, and service management. DCME and Smart
-        Dental will continue to provide the same services to the Client, as per
-        the existing subscription plan, including all features associated with
-        the Platinum plan, if applicable.
-      </p>
-      <p>
-        Clients will continue to have access to all documents stored in Smart
-        Dental until the migration and onboarding to the DCME platform is
-        complete. Smart Dental will continue to support Client information and
-        data migration until onboarding to DCME is complete. Clients will have
-        access to all their information on the Smart Dental platform until the
-        migration to DCME has been completed.
+        You agree that all uses involving your registration identifier are
+        attributed to and legally bind you and may be relied upon by us and our
+        agents, affiliates, and partners.
       </p>
 
       <!-- Section 2 -->
-      <h3 class="terms-heading mt-6">
-        2. Data Privacy & Compliance with UK Law
-      </h3>
+      <h3 class="terms-heading mt-6">2. Our Content</h3>
       <p>The Client acknowledges and agrees that:</p>
       <p>
-        All personal data transferred by the Client to DCME will be processed in
-        compliance with the UK General Data Protection Regulation (GDPR) and
-        other relevant data protection laws applicable in the United Kingdom.
+        The Platform may contain various information, including text, data,
+        software, graphics, videos, tutorials, or other materials (“Content”).
+        You may review, download, copy, and use Content only for personal,
+        non-commercial purposes. Content must not be sold or distributed for a
+        fee or disclosed except as permitted by these Terms.
       </p>
       <p>
-        The Client will update its consent wording and notices to ensure that it
-        reflects the transfer of the personal data to DCME. DCME will act as the
-        data processor and will adhere to all relevant legal obligations
-        concerning data protection, including implementing appropriate security
-        measures to protect the personal data during and after the transfer
-        process.
+        Content may be outdated or contain errors. You agree not to rely solely
+        on Platform Content, and to verify accuracy and other relevant factors
+        yourself. We may monitor, modify, or remove Content any time but do not
+        undertake a duty to do so.
       </p>
       <p>
-        The Client agrees to the continued storage and processing of their
-        personal data by DCME as necessary for the provision of services, in
-        accordance with the terms of the GDPR and UK Data Protection Act 2018.
-      </p>
-      <p>
-        The Client, as controller, has the right to request access to, rectify,
-        or request deletion of relevant personal data, as per the rights granted
-        under GDPR. For the purposes of GDPR, DCME will provide a Data
-        Processing Agreement (DPA) outlining the specific terms under which
-        Client's data will be handled, which the Client agrees to upon migration
-        to the DCME platform.
+        Opinions or information expressed by third parties or users are their
+        own and not those of Flossly.
       </p>
 
       <!-- Section 3 -->
-      <h3 class="terms-heading mt-6">3. Migration</h3>
+      <h3 class="terms-heading mt-6">3. Your Content</h3>
       <p>
-        Once this Transfer Agreement as well as a DCME agreement has been signed
-        and executed, the DCME Onboarding Team and Smart Dental will facilitate
-        the onboarding to the DCME platform.
+        You may upload or provide content (“Your Content”) through use of the
+        Platform. You warrant that Your Content is your original work or that
+        you have rights to share it, and it does not violate intellectual
+        property, privacy, or other rights.
       </p>
       <p>
-        Flossly will support this process by providing Clients with support and
-        assistance if required with regards to the downloading of information
-        from the Smart Dental platform, such that this information can be
-        submitted to DCME as part of the DCME onboarding process.
+        You grant FlosslyOS and its agents a perpetual, nonexclusive, worldwide,
+        royalty-free, irrevocable license to use, copy, distribute, modify,
+        publicly display, create derivatives, and sublicense Your Content as
+        required to enable Platform operation and fulfillment of related
+        services.
       </p>
       <p>
-        The Client agrees to cooperate fully with DCME during this migration by
-        providing DCME with any requested information or assistance to complete
-        the onboarding process to the DCME platform
+        We may remove Your Content if it violates these Terms or presents
+        liability. You acknowledge we may keep Your Content indefinitely and
+        disclose it to comply with law or protect rights and safety.
       </p>
+      <h3 class="terms-heading mt-6">4. Acceptable Use; Suspension</h3>
       <p>
-        Upon signing this Agreement, the Client agrees to allow the DCME
-        Onboarding Team to begin the migration process immediately. This will
-        cover the following:
+        You must use the Platform for its intended purposes only. You may not
+        sell, lease, assign, sublicense, exploit, broadcast, or transfer
+        Platform or Content rights. You may not:
       </p>
       <ul class="ml-6 list-disc">
+        <li>Use Platform in violation of these Terms or laws</li>
+        <li>Post unlawful, harassing, offensive, or infringing material</li>
+        <li>Upload files containing malware or harmful code</li>
+        <li>Collect information about others without consent</li>
         <li>
-          DCME Onboarding team to share new 12 month contract agreement
-          confirming client agreement to join DCME.
-        </li>
-        <li>
-          DCME Onboarding team to share onboarding information collection sheet
-          to set up your DCME platform
-        </li>
-        <li>
-          Once you have been fully onboarded to the DCME platform you and your
-          nominated team members will be invited to a training session to go
-          through all parts, features of your new DCME platform
-        </li>
-        <li>
-          Your access to your smart dental platform will continue until the
-          onboarding process with DCME is complete
+          Attempt unauthorized access or use means other than provided
+          interfaces
         </li>
       </ul>
+      <p>
+        We may terminate or suspend your access without notice at our discretion
+        for breaches or risks, in addition to other remedies permitted by law.
+      </p>
+      <h3 class="terms-heading mt-6">5. Export Controls</h3>
+      <p>
+        You may not use or export Platform or related information in violation
+        of applicable export laws. You confirm you are not subject to any
+        applicable sanctions, prohibited lists, or export restrictions.
+      </p>
+      <h3 class="terms-heading mt-6">6. Errors and Inaccuracies</h3>
+      <p>
+        The Platform, including all Content and services, is provided “As Is”
+        and “When available.” We reserve the right to correct errors, update or
+        change the Platform, Content, or Your Content at any time without
+        notice, but offer no guarantee of correction.
+      </p>
+      <h3 class="terms-heading mt-6">7. Privacy Policy</h3>
+      <p>
+        Please refer to our Privacy Policy, which governs collection, use, and
+        storage of personal data. Do not provide personal data unless you are
+        authorized or required, and ensure explicit consent and sharing of our
+        Privacy Policy with all parties impacted.
+      </p>
+      <h3 class="terms-heading mt-6">8. Intellectual Property Rights</h3>
+      <p>
+        Names, logos, trademarks, service marks (“Marks”), technology, and all
+        modifications or derivatives are property of Flossly and its licensors.
+        All rights not expressly granted are reserved. Do not use Flossly's
+        Marks without written consent.
+      </p>
+      <h3 class="terms-heading mt-6">9. Feedback</h3>
+      <p>
+        Providing feedback (“Feedback”) grants Flossly a non-exclusive,
+        worldwide, irrevocable, royalty-free license to use any intellectual
+        property or other rights in the Feedback for Platform improvement, in
+        all media.
+      </p>
+      <h3 class="terms-heading mt-6">10. Indemnification</h3>
+      <p>
+        You agree to indemnify, defend, and hold harmless Flossly and related
+        parties against all claims, damages, losses, or liabilities arising from
+        (i) your use of the Platform, (ii) Your Content, or (iii) breaches of
+        these Terms. We may control our defense and you may not settle without
+        our consent.
+      </p>
+      <h3 class="terms-heading mt-6">11. No Warranties</h3>
+      <p>
+        You agree to indemnify, defend, and hold harmless Flossly and related
+        parties against all claims, damages, losses, or liabilities arising from
+        (i) your use of the Platform, (ii) Your Content, or (iii) breaches of
+        these Terms. We may control our defense and you may not settle without
+        our consent.
+      </p>
+      <h3 class="terms-heading mt-6">12. Limitation of Liability</h3>
+      <p>
+        TO THE FULL EXTENT PERMITTED BY LAW, FLOSSLY SHALL NOT BE LIABLE FOR ANY
+        SPECIAL, CONSEQUENTIAL, INCIDENTAL, OR PUNITIVE DAMAGES, LOST PROFITS,
+        OR DAMAGES ARISING OUT OF THE PLATFORM OR THESE TERMS.
+      </p>
+      <h3 class="terms-heading mt-6">13. Exclusive Remedy</h3>
+      <p>
+        Your exclusive remedy for any breach or damages is, at our option,
+        substitution or correction of the content/service, or a maximum of £100
+        (or local equivalent). These limits apply regardless of essential
+        purpose failure.
+      </p>
+      <h3 class="terms-heading mt-6">14. Third Party Services; Linked Sites</h3>
+      <p>
+        The Platform may link to, or reference, third party services or
+        websites. Use of such resources is at your own risk and subject to their
+        privacy policies; Flossly is not responsible for third party products or
+        sites. You may not link to the Platform without Flossly's written
+        permission.
+      </p>
+      <h3 class="terms-heading mt-6">15. Amendments</h3>
+      <p>
+        We may modify these Terms, including the Privacy Policy, at any time.
+        You are responsible for checking for updates, and use after changes
+        constitutes acceptance. No amendments other than posted updates unless
+        in writing and signed by both parties.
+      </p>
+      <h3 class="terms-heading mt-6">16. Governing Law and Jurisdiction</h3>
+      <p>
+        These Terms and Platform use are governed exclusively by the laws of
+        England and Wales (or your applicable jurisdiction), and you consent to
+        exclusive jurisdiction of the courts of England and Wales.
+      </p>
+      <h3 class="terms-heading mt-6">17. Notices</h3>
+      <p>
+        We will use your contact details for providing notices regarding these
+        Terms and Platform matters. Notices may be posted on the Platform,
+        emailed, or delivered in other ways, and are deemed received after 24
+        hours of delivery. Notices to us should be sent to support@flossly.co.uk
+        (or your specified contact).
+      </p>
+      <h3 class="terms-heading mt-6">18. Term and Termination</h3>
+      <p>
+        These Terms will apply for as long as you use or maintain a
+        username/account for the Platform. Your subscription agreement is
+        subject to a minimum contract period of twelve (12) months from the
+        initial activation date. During this initial 12-month term, the
+        agreement may not be terminated, downgraded, or upgraded except with at
+        least one (1) month's prior written notice (30 days). After the initial
+        minimum term expires, the contract will continue on a rolling monthly
+        basis unless cancelled by either party giving at least one (1) month's
+        written notice. Upgrades or downgrades of the subscription plan are also
+        subject to one month's notice. We may deny or suspend access, or
+        terminate or cancel these Terms, with or without cause and at any time
+        and without prior notice in accordance with the other termination
+        provisions detailed herein. Any termination or cancellation will not
+        eliminate the survival of provisions of these Terms and you remain
+        liable for all obligations incurred before termination or cancellation.
+      </p>
+      <h3 class="terms-heading mt-6">20. Data Download and Transfer</h3>
+      <p>
+        Upon termination, cancellation, or expiry of the Agreement for any
+        reason, it is the user's sole responsibility to download or export all
+        their data from the Platform prior to the effective termination date.
+        FlosslyOS will not be responsible for retaining or providing user data
+        after termination, except as required by law; and shall have no
+        liability for loss of data if the user has not downloaded their data
+        before access ends. In the event that FlosslyOS Ltd. is sold, merges, or
+        otherwise transfers Platform ownership, users will be notified and
+        required to download or migrate their own data prior to the changeover.
+        Following the transfer, users must move any required data to the new
+        company's systems at their own cost and responsibility. Any continued
+        use after transfer will be governed by the terms and conditions of the
+        new entity.
+      </p>
+      <h3 class="terms-heading mt-6">
+        21. Data Protection and GDPR Compliance
+      </h3>
+      <p>
+        As Flossly processes sensitive personal data including patient names,
+        dates of birth, addresses, and appointment information, the Platform is
+        subject to and compliant with UK GDPR and relevant data protection laws.
+        FlosslyOS will only process patient data for legitimate purposes
+        directly relating to healthcare service provision, appointment booking,
+        and clinic administration. We implement appropriate technical and
+        organisational measures to ensure the security and confidentiality of
+        sensitive data, including encryption, access controls, and secure
+        storage.
+      </p>
+      <p>
+        Users are responsible for obtaining and recording all necessary lawful
+        bases for processing patient data, including valid consent where
+        required, and for notifying patients how their information will be used
+        in accordance with GDPR transparency obligations. Patients retain the
+        right to access, correct, restrict, erase, and object to processing of
+        their personal data, and may request data portability. FlosslyOS will
+        cooperate promptly with all user requests for support in exercising
+        these rights, including supplying and deleting patient records as
+        required by law.
+      </p>
+      <p>
+        In the event of a personal data breach involving patient information,
+        FlosslyOS will notify affected users and regulators as required by GDPR
+        and provide all reasonable assistance in managing and responding to the
+        incident. Data processing and privacy procedures will be regularly
+        reviewed and updated to ensure ongoing compliance
+      </p>
     </v-card-text>
   </v-container>
 </template>
