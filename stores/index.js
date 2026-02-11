848338b5eaf70154c4c83df8992042cfe859ca80
Taha Iqbal <tahaiqbal001@gmail.com>
2026-02-11 19:13:32 +0500

    made changes

--- stores/index.js
+++ stores/index.js
@@ -22,6 +22,7 @@ const LICENSE_FEATURES = {
     "docs",
     "team",
     "crm",
+    diary
   ]),
   [LICENSE_TYPES.DRIFT]: new Set(["dashboard", "tasks", "docs"]),
   [LICENSE_TYPES.GLIDE]: new Set(["dashboard", "tasks", "docs", "team", "crm"]),
