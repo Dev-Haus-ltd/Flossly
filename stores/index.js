ff05d2d149b57d8f311f48db83bbce91fff48f1f
Taha Iqbal <tahaiqbal001@gmail.com>
2026-03-27 21:09:19 +0500

    made diary visible for dev

--- stores/index.js
+++ stores/index.js
@@ -23,7 +23,7 @@ const LICENSE_FEATURES = {
     "docs",
     "team",
     "crm",
-    // "diary",
+    "diary",
   ]),
   [LICENSE_TYPES.DRIFT]: new Set(["dashboard", "tasks", "docs"]),
   [LICENSE_TYPES.GLIDE]: new Set(["dashboard", "tasks", "docs", "team", "crm"]),
@@ -41,7 +41,7 @@ const LICENSE_FEATURES = {
     "docs",
     "team",
     "crm",
-    "diary",
+    // "diary",
   ]),
 };
 
