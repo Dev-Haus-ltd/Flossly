3c51309955dff93520b29eadcd174d7abaca01fc
Taha Iqbal <tahaiqbal001@gmail.com>
2026-03-30 15:38:38 +0500

    fixed

--- components/customerRelationManagement/overview.vue
+++ components/customerRelationManagement/overview.vue
@@ -568,6 +568,16 @@ const gscChartConfig = reactive({
   summaryStats: []
 })
 
+const googleStatus = reactive({
+  connected: false,
+  email: '',
+  tokenId: '',
+  tokenValid: false,
+  connectedAt: '',
+  expiresAt: '',
+  scopes: [],
+})
+
 const metaHealthDialog = ref(false)
 const metaHealthLoading = ref(false)
 const metaHealthData = ref(null)
