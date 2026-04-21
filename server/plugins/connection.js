e456fd18ad9d41d8da938352c45ca11b0a424b40
Taha Iqbal <tahaiqbal001@gmail.com>
2026-04-21 07:24:06 +0500

    fix

--- server/plugins/connection.js
+++ server/plugins/connection.js
@@ -1,9 +1,12 @@
 import "../models";
-import sequelize  from "../utils/db";
+import sequelize from "../utils/db";
+import { seedCrmAutomationDictionary } from "../utils/seedCrmAutomationDictionary";
 export default defineNitroPlugin(async (nitroApp) => {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
+    await seedCrmAutomationDictionary();
+    console.log("CRM automation dictionary seeded.");
     //  await sequelize.sync({ alter: true });
     //  console.log("Models synced successfully.");
   } catch (error) {
